d016c8aa027bcad2bc7d1c5657b00aea8e7145cc
sakesuki <67051056+sakesuki@users.noreply.github.com>
2020-07-22 00:14:10 +0900

    initial commit
    初投稿です

---
+++ dbc.php
@@ -0,0 +1,99 @@
+<?php
+
+ini_set('display_errors',On);
+
+
+Class Dbc 
+{
+    protected $table_name;
+    
+    //dbに接続
+    public function dbConnect(){
+      $dsn = 'mysql:host=localhost;dbname=tsumami_rog;charset=utf8';
+      $user= 'tsumami_user';
+      $pass= '08120337';
+      try{
+        $dbh = new \PDO($dsn,$user,$pass,[
+          \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+        ]); 
+        }catch(PDOException $e){
+        echo 'error' . $e->getMeage();
+        exit();
+        };
+        return $dbh;
+        }
+
+    //dbから情報を取得
+    public function getAlltsumami(){
+      $dbh = $this->dbConnect();
+      $sql = "SELECT*FROM tsumamirog";
+      $stmt = $dbh->query($sql);
+      $result = $stmt->fetchall(\PDO::FETCH_ASSOC);
+      return $result;
+      $dbh = null;
+     }
+
+
+    //カテゴリー名のセット
+    public function setCategoryName($category){
+    if($category === '1'){
+      return 'ビールにあう';
+    }elseif($category ==='2'){
+      return 'ワインにあう';
+    }elseif($category ==='3'){
+      return 'ウイスキーにあう';
+    }elseif($category ==='4'){
+      return '焼酎にあう';
+    }else{
+      return 'つまみ';
+    }
+}
+
+
+    //dbのID
+    public function getRog($id){
+      if(empty($id)){
+        exit('IDが不正です');
+      }
+        $dbh = $this->dbConnect();
+        $stmt = $dbh->prepare('SELECT * FROM tsumamirog where id = :id');
+        $stmt->bindValue(':id',(int)$id,\PDO::PARAM_INT);
+      
+        $stmt->execute();
+        $result = $stmt->fetch(\PDO::FETCH_ASSOC);
+        //  var_dump($result);
+      
+        if(!$result){
+          exit('記事がありません');
+        }
+        return $result;
+     }
+
+     
+  public function logCreate($blogs){
+        $sql = 'INSERT INTO
+        tsumamirog(title,users,category,content)
+        VALUES
+        (:title,:users,:category,:content)';
+
+    $dbh = $this->dbConnect();
+    $dbh->beginTransaction();
+    try{
+      $stmt = $dbh->prepare($sql);
+      $stmt->bindValue(':title',$blogs['タイトル'],PDO::PARAM_STR);
+      $stmt->bindValue(':users',$blogs['ユーザー名'],PDO::PARAM_STR);
+      $stmt->bindValue(':category',$blogs['カテゴリー'],PDO::PARAM_INT);
+      $stmt->bindValue(':content',$blogs['投稿内容'],PDO::PARAM_STR);
+      $stmt->execute();
+      $dbh->commit();
+      echo '投稿しました';
+    }catch(PDOException $e){
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $dbh->rollBack();
+      exit ($e);
+}
+
+}
+
+} 
+?>
---
+++ index_create.php
@@ -0,0 +1,41 @@
+<?php
+require_once('dbc.php');
+$blogs = $_POST;
+
+//投稿内容に不備があった歳の処理
+
+if(empty($blogs['タイトル'])){
+  exit('タイトルを入力してください');
+}
+
+if(mb_strlen($blogs['タイトル'])>191){
+  exit('タイトルは191文字以下で入力してください');
+}
+
+if(empty($blogs['ユーザー名'])){
+  exit('投稿者名を入力してください');
+}
+
+if(empty($blogs['投稿内容'])){
+  exit('本文を入力してください');
+}
+
+if(empty($blogs['カテゴリー'])){
+  exit('カテゴリーは必須です');
+}
+
+$dbc = new Dbc();
+$dbc->logCreate($blogs);
+?>
+<!DOCTYPE html>
+<html lang=ja>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>新規投稿</title>
+</head>
+<body>
+  <br>
+  <a href="index.php">戻る</a>
+</body>
+</html>
