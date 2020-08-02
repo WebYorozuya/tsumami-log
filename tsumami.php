d3f6d2389f19514fb4521458b805aa209707ed39
sakesuki <xxxmoriyu@yahoo.co.jp>
2020-08-02 23:12:30 +0900

    second commit

---
+++ tsumami.php
@@ -0,0 +1,97 @@
+<?php
+require_once('dbc.php');
+
+Class Tsumami extends dbc
+{
+
+    protected $table_name = 'tsumamilog';
+    //カテゴリー名のセット
+    public function setCategoryName($category){
+      if($category === '1') {
+        return 'ビールにあう';
+      }elseif($category ==='2') {
+        return 'ワインにあう';
+      }elseif($category ==='3') {
+        return 'ウイスキーにあう';
+      }elseif($category ==='4') {
+        return '焼酎にあう';
+      }else{
+        return 'つまみ';
+      }
+    }
+
+
+    public function logCreate($blogs){
+      $sql = "INSERT INTO
+                  $this->table_name(title,users,category,content)
+              VALUES
+                 (:title,:users,:category,:content)";
+  
+      $dbh = $this->dbConnect();
+      $dbh->beginTransaction();
+      try{
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(':title',$blogs['タイトル'],PDO::PARAM_STR);
+        $stmt->bindValue(':users',$blogs['ユーザー名'],PDO::PARAM_STR);
+        $stmt->bindValue(':category',$blogs['カテゴリー'],PDO::PARAM_INT);
+        $stmt->bindValue(':content',$blogs['投稿内容'],PDO::PARAM_STR);
+        $stmt->execute();
+        $dbh->commit();
+        echo '投稿しました';
+
+      }catch(PDOException $e){
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbh->rollBack();
+        exit ($e);
+        }
+      }
+
+    public function logUpdate($blogs){
+        $sql = "UPDATE $this->table_name SET
+        title=:title,users=:users,category=:category,content=:content
+        WHERE
+          id=:id";
+
+        $dbh = $this->dbConnect();
+        $dbh->beginTransaction();
+        try{
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(':title',$blogs['タイトル'],PDO::PARAM_STR);
+        $stmt->bindValue(':users',$blogs['ユーザー名'],PDO::PARAM_STR);
+        $stmt->bindValue(':category',$blogs['カテゴリー'],PDO::PARAM_INT);
+        $stmt->bindValue(':content',$blogs['投稿内容'],PDO::PARAM_STR);
+        $stmt->bindValue(':id',$blogs['id'],PDO::PARAM_INT);
+        $stmt->execute();
+        $dbh->commit();
+        echo '投稿しました';
+
+        }catch(PDOException $e){
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbh->rollBack();
+        exit ($e);
+}
+      }
+
+
+
+    //ブログのバリデーション
+      public function logValidate($blogs){
+        if(empty($blogs['タイトル'])){
+          exit('タイトルを入力してください');
+        }
+        if(mb_strlen($blogs['タイトル'])>191){
+          exit('タイトルは191文字以下で入力してください');
+        }
+        if(empty($blogs['ユーザー名'])){
+          exit('投稿者名を入力してください');
+        }
+        if(empty($blogs['投稿内容'])){
+          exit('本文を入力してください');
+        }
+        if(empty($blogs['カテゴリー'])){
+          exit('カテゴリーは必須です');
+        }
+      }
+}
+
+?>
