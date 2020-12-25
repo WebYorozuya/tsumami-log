94b24c6be9fc6c004dee0d913b93ffd7eb0b1b99
sakesuki <xxxmoriyu@yahoo.co.jp>
2020-12-26 07:47:58 +0900

    インデント修正

--- dbconnect.php
+++ dbconnect.php
@@ -10,22 +10,22 @@ $dotenv = Dotenv::createImmutable(__DIR__);
 //.envファイルから環境変数から読み込み
 $dotenv->load();
 
-function connect() {
-    $host = $_ENV['DB_HOST'];
-    $db   = $_ENV['LOGINDB_NAME'];
-    $user = $_ENV['LOGINDB_USER'];
-    $pass = $_ENV['LOGINDB_PASS'];
-    $dsn  = "mysql:host=$host;dbname=$db;charset=utf8mb4";
-  
-    try{
-       $pdo  = new PDO($dsn,$user,$pass,[
-          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-          ]);
-       return $pdo;
-     } catch(PDOExeption $e) {
-        echo '接続失敗です'.$e->getMessage();
-        exit();
-     }
+function connect()
+{
+   $host = $_ENV['DB_HOST'];
+   $db   = $_ENV['LOGINDB_NAME'];
+   $user = $_ENV['LOGINDB_USER'];
+   $pass = $_ENV['LOGINDB_PASS'];
+   $dsn  = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+   try {
+      $pdo  = new PDO($dsn, $user, $pass, [
+         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      ]);
+      return $pdo;
+   } catch (PDOExeption $e) {
+      echo '接続失敗です' . $e->getMessage();
+      exit();
+   }
 }
-?>
