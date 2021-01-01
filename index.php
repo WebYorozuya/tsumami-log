f32ff80fc9c87e0910a386dd1dacff179aa84a2c
sakesuki <xxxmoriyu@yahoo.co.jp>
2021-01-01 22:56:23 +0900

    お問い合わせフォーム作成

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
   $tsumami = new Tsumami('tsumamilog');
 
   //dbからつまみログのデータを取得
-  // $tsumamiData = $tsumami->getAll();
   $tsumamiData = $tsumami->getPages();
   $pages = $tsumami->countPage();
   $page = $_REQUEST['page'];
@@ -16,6 +15,7 @@
 
   <!DOCTYPE html>
   <html lang="ja">
+
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -28,10 +28,10 @@
   </head>
 
   <body>
-    <!-- ヘッダー -->
+    <!-- start nav -->
     <header class="header">
       <div class="main_menu">
-        <nav class="navbar-dark bg-dark navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <nav class="navbar-dark bg-dark navbar navbar-expand-lg text-dark fixed-top">
           <a class="navbar-brand" href="#"><i class="fas fa-beer"></i></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span></button>
@@ -39,28 +39,33 @@
             <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
               <li class="nav-item">
-                <a class="nav-link" href="#new-content">新規投稿</a></li>
-              <a class="nav-link " href="index.html">投稿する<span class="sr-only">(current)</span></a></li>
+                <a class="nav-link text-light pr-4" href="#new-content">新規投稿</a></li>
+              <a class="nav-link text-light pr-4" href="index.html">投稿する<span class="sr-only">(current)</span></a></li>
               <li class="nav-item">
-                <a class="nav-link" href="./public/signup_form.php">ユーザ登録</a></li>
+                <a class="nav-link text-light pr-4" href="./public/signup_form.php">ユーザ登録</a></li>
               <li class="nav-item">
-                <a class="nav-link" href="./public/login.php">ログイン</a></li>
+                <a class="nav-link text-light" href="./public/login.php">ログイン</a></li>
             </ul>
           </div>
         </nav>
     </header>
-    <img src="./img/tsumami-top.png" class="rounded mx-auto d-block container-lg img-fluid" alt="Responsive image">
-    </div>
+    <!-- end nav-->
 
-    <div class="container bg-white py-5 my-5">
-      <!-- タブ -->
+    <!-- start top-->
+    <div>
+      <!-- <img src="./img/tsumami-top.png" class="rounded mx-auto d-block img-fluid" alt="Responsive image"> -->
+      <img src="./img/sakesample.jpg" class="img-fluid rounded mx-auto d-block" alt="Responsive image">
+    </div>
+    <!-- end top-->
+    <!-- start tab -->
+    <div class="container bg-white py-5 my-3">
       <div class="tav">
         <ul class="nav nav-tabs" id="myTab" role="tablist">
           <li class="nav-item col-6 p-0" role="presentation">
-            <a class="nav-link active text-center" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">つまみログとは？</a>
+            <a class="nav-link active text-center text-dark" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">つまみログとは？</a>
           </li>
           <li class="nav-item col-6 p-0" role="presentation">
-            <a class="nav-link text-center" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+            <a class="nav-link text-center text-dark" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
           </li>
         </ul>
         <div class="tab-content" id="myTabContent">
@@ -88,10 +93,10 @@
           </div>
         </div>
       </div>
+      <!-- end tab -->
 
-      <!-- 投稿部分-->
-      <h2 id="new-content">新規投稿一覧</h2>
-      <div class="container">
+      <div class="container pt-5">
+        <h2 id="new-content" class="p-3 mx-auto text-dark bg-light text-center rounded-pill" style="width: 200px;">新規投稿一覧</h2>
         <div class="row">
           <?php foreach ($tsumamiData as $column) : ?>
             <div class="col-md-4 col-12">
@@ -100,7 +105,7 @@
                   <img src="<?php echo h($column['images']) ?>" class="card-img-top" alt="...">
                   <h5 class="card-title"><?php echo h($column['title']) ?></h5>
                   <p class="card-text"><?php echo h($column['content']) ?></p>
-                  <a class="btn btn-primary stretched-link" href="detail.php?id=<?php echo $column['id'] ?>">詳細</a>
+                  <a class="btn btn-light stretched-link" href="detail.php?id=<?php echo $column['id'] ?>">詳細</a>
                 </div>
               </div>
             </div>
@@ -108,13 +113,10 @@
         </div>
       </div>
     </div>
-
+    <!-- end contents -->
     <!--ページネーション-->
     <nav aria-label="Page navigation example">
       <ul class="pagination justify-content-center">
-        <!-- <li class="page-item disabled">
-      <a class="page-link" href="index.php" tabindex="-1" aria-disabled="true">Previous</a>
-    </li> -->
         <?php if ($page >= 2) : ?>
           <li class="page-item"><a class="page-link" href="index.php?page=<?php print($page - 1); ?>">前←</a></li>
         <?php endif; ?>
@@ -122,9 +124,6 @@
         <?php if ($page < $pages) : ?>
           <li class="page-item"><a class="page-link" href="index.php?page=<?php print($page + 1); ?>">次→</a></li>
         <?php endif; ?>
-        <!-- <li class="page-item">
-      <a class="page-link" href="#">Next</a>
-    </li> -->
       </ul>
     </nav>
     <!--フッター部分-->
@@ -138,7 +137,7 @@
             <div class="mx-5">
               <img class="img-fluid rounded-circle" src="./img/profile.png" alt="">
             </div>
-            <p>お酒が好きな31歳、R1.11月〜プログラミング学習を始めました。将来はwebエンジニア転職を目指していきたいです。
+            <p class="pt-3">お酒が好きな31歳、R1.11月〜プログラミング学習を始めました。将来は福祉業界をテクノロジーの力で変えていく仕事をしたい
           </div>
           <div class="col-md-4 col-12">
             <div class="py-4">
@@ -146,8 +145,8 @@
             </div>
             <div class="mx-5">
               <ul class="list-group list-group-flush">
-                <li class="list-group-item"><a href="https://sakesuki.github.io/frontend-dev/">Sass/JavaScriptを使用したWEBページ</a></li>
-                <li class="list-group-item"><a href="https://sakesuki.github.io/TypingGame/">タイピングゲーム</a></li>
+                <li class="list-group-item"><a href="https://sakesuki.github.io/frontend-dev/" class="text-dark">Sass/JavaScriptを使用したWEBページ</a></li>
+                <li class="list-group-item"><a href="https://sakesuki.github.io/TypingGame/" class="text-dark">タイピングゲーム</a></li>
               </ul>
             </div>
           </div>
