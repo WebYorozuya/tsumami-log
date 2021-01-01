289d6569a2c896e5c7c0b16208b55cf0801f75ec
sakesuki <xxxmoriyu@yahoo.co.jp>
2021-01-01 22:53:16 +0900

    インデント修正

--- index_create.php
+++ index_create.php
@@ -3,7 +3,7 @@ require_once(dirname(__FILE__) . '/tsumami.php');
 require_once(dirname(__FILE__) . '/s3upload.php');
 
 $blogs = $_POST;
-  //ファイル関連取得
+//ファイル関連取得
 $file = $_FILES['image'];
 $filename = basename($file['name']);
 $tmp_path = $file['tmp_name'];
@@ -11,18 +11,21 @@ $save_filename = date('YmdHis') . $filename;
 $save_path = s3getObject($save_filename);
 $tsumami = new Tsumami();
 $tsumami->logValidate($blogs);
-$tsumami->logCreate($blogs ,$save_path);
+$tsumami->logCreate($blogs, $save_path);
 ?>
 
 <!DOCTYPE html>
 <html lang=ja>
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>新規投稿</title>
 </head>
+
 <body>
   <br>
   <a href="index.php">戻る</a>
 </body>
+
 </html>
